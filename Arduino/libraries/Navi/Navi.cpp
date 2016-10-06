f5b2105e2d0edd596b3e0633b78f5876b2c37e18
amicah <a.micah.mcclain@gmail.com>
2016-10-06 16:29:32 -0700

    Add files via upload

--- Arduino/libraries/Navi/Navi.cpp
+++ Arduino/libraries/Navi/Navi.cpp
@@ -1,1 +1,251 @@
-
+/*  Description: Controls Other Objects Related to Movement
+	Author: A Micah McClain
+	Email: a.micah.mcclain@gmail.com
+	Date: 02/06/16
+*/
+#include "Arduino.h"
+#include "AGSens.h"
+#include "IRSens.h"
+#include "Motor.h"
+#include "Navi.h"
+#define maxPWM 255
+bool fwd = true;
+Navi::Navi() { }
+Navi::~Navi() { }
+void Navi::init() {
+	if (aGConnected) {
+		getAG()->init();
+		getAG()->update();
+		setPosX(0);
+		setPosY(0);
+	}
+	if (mLConnected || mRConnected) {
+		getML()->init();
+		getMR()->init();
+		getML()->setPWM(maxPWM);
+		getMR()->setPWM(maxPWM);
+		setLeftC(maxPWM);
+		setRightC(maxPWM);
+		setFwd();
+	}
+}
+void Navi::debug() {
+	if (sFConnected) {
+		Serial.print("f");
+		getSF()->printOut();
+		Serial.print("\t");
+	}
+	if (sBConnected) {
+		Serial.print("b");
+		getSB()->printOut();
+		Serial.print("\t");
+	}
+	if (sLConnected) {
+		Serial.print("l");
+		getSL()->printOut();
+		Serial.print("\t");
+	}
+	if (sRConnected) {
+		Serial.print("r");
+		getSR()->printOut();
+		Serial.print("\t");
+	}
+	if (mLConnected) {
+		Serial.print("l");
+		getML()->printData();
+		getML()->printPWM();
+		Serial.print("\t");
+	}
+	if (mRConnected) {
+		Serial.print("r");
+		getMR()->printData();
+		getMR()->printPWM();
+		Serial.print("\t");
+	}
+	if (aGConnected) {
+		Serial.print("Acc: ");
+		getAG()->printAccel();
+		Serial.print("\t");
+	}
+}
+void Navi::sense() {
+	// Read IR Sensor Data
+	if (sFConnected) {
+		getSF()->update();
+	}
+	if (sBConnected) {
+		getSB()->update();
+	}
+	if (sLConnected) {
+		getSL()->update();
+	}
+	if (sRConnected) {
+		getSR()->update();
+	}
+	// Read Accel/Gyro Sensor Data
+	if (aGConnected) {
+		updatePosition();
+	}
+}
+void Navi::printPosition() {
+	if (aGConnected) {
+		Serial.print("Pos: ");
+		Serial.print(getPosX());
+		Serial.print(",");
+		Serial.print(getPosY());
+		Serial.print("\t");
+	}
+}
+void Navi::updatePosition() {
+	int tempX1, tempY1, tempX2, tempY2;
+	tempX1 = getAG()->getAX();
+	tempY1 = getAG()->getAY();
+	getAG()->update();
+	tempX2 = getAG()->getAX();
+	tempY2 = getAG()->getAY();
+	if (tempX2 >= (tempX1 + 5) || tempX2 <= (tempX1 - 5)) {
+		setPosX(getPosX() - (tempX1 - tempX2));
+	}
+	if (tempY2 >= (tempY1 + 5) || tempY2 <= (tempY1 - 5)) {
+		setPosY(getPosY() - (tempY1 - tempY2));
+	}
+}
+void Navi::go() {
+	drive();
+	correction();
+	//=======Test with 2 Sensors========
+	if (getSF()->sensObj()) {
+		turnAround();
+	}
+	//=======Test with 2 Sensors========
+	//// If the sensor is reading correctly
+	//if (getSF()->getOut() > 0) {
+	//	// If the Front sensor sees an object
+	//	if (getSF()->sensObj()) {
+	//		// If the Left sensor sees and object
+	//		if (getSL()->sensObj()) {
+	//			// If the Right Sensor sees and object
+	//			if (getSR()->sensObj()) {
+	//				turnAround(); // Turn around
+	//			}
+	//			// If the Right sensor sees nothing
+	//			else {
+	//				turnRight(); // Turn right
+	//			}
+	//		}
+	//		// If the Left sensor sees nothing
+	//		else {
+	//			// If the Right sensor sees an object
+	//			if (getSR()->sensObj()) {
+	//				turnLeft(); // Turn left
+	//			}
+	//		}
+	//	}
+	//}
+}
+void Navi::drive() {
+	getML()->spin();
+	getMR()->spin();
+}
+void Navi::cease() {
+	getML()->halt();
+	getMR()->halt();
+}
+void Navi::correction() {
+	// Avoid Left/Right Wall Collisions
+	setLeftC(maxPWM * (getSR()->getOut()) / 1023);
+	setRightC(maxPWM * (getSL()->getOut()) / 1023);
+	if (getSR()->getOut() < 975) {
+		getML()->setPWM(0);
+	}
+	else {
+		getML()->setPWM(maxPWM);
+	}
+	if (getSL()->getOut() < 975) {
+		getMR()->setPWM(0);
+	}
+	else {
+		getMR()->setPWM(maxPWM);
+	}
+}
+void Navi::setFwd() {
+	getML()->forward();
+	getMR()->forward();
+}
+void Navi::setRev() {
+	getML()->reverse();
+	getMR()->reverse();
+}
+void Navi::setSpin() {
+	getML()->forward();
+	getMR()->reverse();
+}
+void Navi::turnLeft() {
+	getML()->halt();
+	getMR()->spin();
+}
+void Navi::turnRight() {
+	getML()->spin();
+	getMR()->halt();
+}
+void Navi::turnAround() { // Reverse bot
+	cease();
+	// Swap Front and Back IRSens
+	getSF()->swap(getSB());
+	// Swap Left and Right IRSens
+	getSL()->swap(getSR());
+	// Swap Left and Right Motors
+	getML()->swap(getMR());
+	// Set Motors to Go Backwards
+	if (fwd) {
+		setRev();
+		fwd = false;
+	}
+	else {
+		setFwd();
+		fwd = true;
+	}
+}
+AGSens* Navi::getAG() const { return aG; }
+IRSens* Navi::getSF() const { return sF; }
+IRSens* Navi::getSB() const { return sB; }
+IRSens* Navi::getSL() const { return sL; }
+IRSens* Navi::getSR() const { return sR; }
+Motor* Navi::getML() const { return mL; }
+Motor* Navi::getMR() const { return mR; }
+int Navi::getLeftC() const { return leftC; }
+int Navi::getRightC() const { return rightC; }
+int Navi::getPosX() const { return posX; }
+int Navi::getPosY() const { return posY; }
+void Navi::setAG(AGSens* obj) {
+	aG = obj;
+	aGConnected = true;
+}
+void Navi::setSF(IRSens* obj) {
+	sF = obj;
+	sFConnected = true;
+}
+void Navi::setSB(IRSens* obj) {
+	sB = obj;
+	sBConnected = true;
+}
+void Navi::setSL(IRSens* obj) {
+	sL = obj;
+	sLConnected = true;
+}
+void Navi::setSR(IRSens* obj) {
+	sR = obj;
+	sRConnected = true;
+}
+void Navi::setML(Motor* obj) {
+	mL = obj;
+	mLConnected = true;
+}
+void Navi::setMR(Motor* obj) {
+	mR = obj;
+	mRConnected = true;
+}
+void Navi::setLeftC(int n) { leftC = n; }
+void Navi::setRightC(int n) { rightC = n; }
+void Navi::setPosX(int n) { posX = n; }
+void Navi::setPosY(int n) { posY = n; }
---
+++ Arduino/libraries/Navi/Navi.h
@@ -0,0 +1,78 @@
+/*  Description: Controls Other Objects Related to Movement
+	Author: A Micah McClain
+	Email: a.micah.mcclain@gmail.com
+	Date: 01/24/16
+*/
+#include "Arduino.h"
+#include "AGSens.h"
+#include "IRSens.h"
+#include "Motor.h"
+#ifndef Navi_h
+#define Navi_h
+class Navi {
+public:
+	// Constructors
+	Navi();
+	// Destructor
+	~Navi();
+	// Functions
+	void init();	// Initializes I/O for Motors
+	void debug();	// Print Values for Debug Purposes
+	void sense();		// Update sensors
+	void printPosition();	// Prints Current x,y Position
+	void updatePosition();	// Update Current x,y Position
+	void go();		// Normal Operation Functions for Loop
+	void drive();	// Begins Spin of Motors
+	void cease();	// Ceases Spin of Motors
+	void correction();	// Course Correction Avoids Side Collisions
+	void setFwd();	// Sets Motors to Spin Forwards
+	void setRev();	// Sets Motors to Spin Backwards
+	void setSpin();	// Sets Motors to Spin Opposite
+	void turnLeft();	// Slows Left Motor for Left Turn
+	void turnRight();	// Slows Right Motor for Right Turn
+	void turnAround();	// Turn Around to Avoid Front Collision
+	// Accessors
+	AGSens* getAG() const;	// Returns AGSens* @object aG
+	IRSens* getSF() const;	// Returns IRSens* @object sF
+	IRSens* getSB() const;	// Returns IRSens* @object sB
+	IRSens* getSL() const;	// Returns IRSens* @object sL
+	IRSens* getSR() const;	// Returns IRSens* @object sR
+	Motor* getML() const;	// Returns Motor* @object mL
+	Motor* getMR() const;	// Returns Motor* @object mR
+	int getLeftC() const;	// Returns int @param leftC
+	int getRightC() const;	// Returns int @param rightC
+	int getPosX() const;	// Returns int @param posX
+	int getPosY() const;	// Returns int @param posY
+	// Mutators
+	void setAG(AGSens*);
+	void setSF(IRSens*);	// Sets IRSens* @object sF
+	void setSB(IRSens*);	// Sets IRSens* @object sB
+	void setSL(IRSens*);	// Sets IRSens* @object sL
+	void setSR(IRSens*);	// Sets IRSens* @object sR
+	void setML(Motor*);		// Sets Motor* @object mL
+	void setMR(Motor*);		// Sets Motor* @object mR
+	void setLeftC(int);		// Sets int @param leftC
+	void setRightC(int);	// Sets int @param rightC
+	void setPosX(int);		// Sets int @param posX
+	void setPosY(int);		// Sets int @param posY
+private:
+	AGSens* aG;	// Accelerometer/Gyro Sensor
+	bool aGConnected = false;
+	IRSens* sF;	// Front IR Sensor
+	bool sFConnected = false;
+	IRSens* sB;	// Back IR Sensor
+	bool sBConnected = false;
+	IRSens* sL;	// Left IR Sensor
+	bool sLConnected = false;
+	IRSens* sR;	// Right IR Sensor
+	bool sRConnected = false;
+	Motor* mL;	// Left DC Motor
+	bool mLConnected = false;
+	Motor* mR;	// Right DC Motor
+	bool mRConnected = false;
+	int leftC;	// Left Course Correction Value
+	int rightC;	// Right Course Correction Value
+	int posX;	// Current X-Axis Position
+	int posY;	// Current Y-Axis Position
+};
+#endif
